9c7ce4e44523ffed08974920ae1156b2302fb8b0
Caleb Evans <caleb@calebevans.me>
2023-02-05 21:50:37 -0800

    Apply Prettier to all files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
-    import { browser } from '$app/environment';
-    import Grid from './Grid.svelte';
+  import { browser } from '$app/environment';
+  import Grid from './Grid.svelte';
 </script>
 
 <h1>Background Generator</h1>
 
 {#if browser}
-    <Grid />
+  <Grid />
 {/if}
 
 <style lang="scss">
-    
-    :global(body) {
-        min-height: 100%;
-        margin: 0;
-        padding: 0 40px;
-        font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-        background-color: #222;
-        text-align: center;
-        color: #fff;
-    }
-    :global(h1) {
-        margin: 20px 0;
-    }
-    :global(a) {
-        color: inherit;
-    }
-
+  :global(body) {
+    min-height: 100%;
+    margin: 0;
+    padding: 0 40px;
+    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+      Arial, sans-serif;
+    background-color: #222;
+    text-align: center;
+    color: #fff;
+  }
+  :global(h1) {
+    margin: 20px 0;
+  }
+  :global(a) {
+    color: inherit;
+  }
 </style>
--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,44 +1,44 @@
 <script lang="ts">
-    import { times } from 'lodash-es';
-    interface GridSquare {
-        x: number;
-        y: number;
-        width: number;
-        height: number;
-    }
-    let gridTiles: GridSquare[] = [];
-    const canvasWidth = 100;
-    const canvasHeight = canvasWidth;
-    const tileWidth = 10;
-    const tileHeight = tileWidth;
-    const tileCount = canvasWidth * canvasHeight / tileWidth / tileHeight;
-    function generateRandomOpacity() {
-        return Math.random() * 0.5;
-    }
-    function generateGridTiles(): GridSquare[] {
-        return times(tileCount, (i: number) => {
-            return {
-                x: (i * tileWidth) % canvasWidth,
-                y: Math.floor(i / tileHeight) * tileHeight,
-                width: tileWidth,
-                height: tileHeight,
-                fill: `rgba(0, 0, 0, ${generateRandomOpacity()})`
-            }
-        });
-    }
-    $: gridTiles = generateGridTiles();
+  import { times } from 'lodash-es';
+  interface GridSquare {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  }
+  let gridTiles: GridSquare[] = [];
+  const canvasWidth = 100;
+  const canvasHeight = canvasWidth;
+  const tileWidth = 10;
+  const tileHeight = tileWidth;
+  const tileCount = (canvasWidth * canvasHeight) / tileWidth / tileHeight;
+  function generateRandomOpacity() {
+    return Math.random() * 0.5;
+  }
+  function generateGridTiles(): GridSquare[] {
+    return times(tileCount, (i: number) => {
+      return {
+        x: (i * tileWidth) % canvasWidth,
+        y: Math.floor(i / tileHeight) * tileHeight,
+        width: tileWidth,
+        height: tileHeight,
+        fill: `rgba(0, 0, 0, ${generateRandomOpacity()})`
+      };
+    });
+  }
+  $: gridTiles = generateGridTiles();
 </script>
 
 <svg viewBox="0 0 {canvasWidth} {canvasHeight}" width="100%">
-    <rect x="0" y="0" width="100%" height="100%" fill="#068" />
-    {#each gridTiles as gridSquare}
-        <rect {...gridSquare} />
-    {/each}
+  <rect x="0" y="0" width="100%" height="100%" fill="#068" />
+  {#each gridTiles as gridSquare}
+    <rect {...gridSquare} />
+  {/each}
 </svg>
 
 <style>
-    svg {
-        width: 100%;
-        max-height: calc(100vh - 120px);
-    }
+  svg {
+    width: 100%;
+    max-height: calc(100vh - 120px);
+  }
 </style>
