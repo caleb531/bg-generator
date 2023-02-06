52cebdb6e097f5dc88a8c7fc7f8a287dc3caf189
Caleb Evans <caleb@calebevans.me>
2023-02-06 10:49:04 -0800

    Correct gridSquare to gridTile

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -33,8 +33,8 @@
 
 <svg viewBox="0 0 {canvasWidth} {canvasHeight}" width="100%">
   <rect x="0" y="0" width="100%" height="100%" fill="#068" />
-  {#each gridTiles as gridSquare}
-    <rect {...gridSquare} />
+  {#each gridTiles as gridTile}
+    <rect {...gridTile} />
   {/each}
 </svg>
 
