caleb531/bg-generator

commit aa4f52aa0bee2548927bf771b19fec4829718e3f

Change localStorage key
This ensures that the pre-1.0 app's localStorage data does not conflict
with the new app.

src/stores/Grid.ts
@@ -212,7 +212,7 @@ export function saveGrid(): void {
     return;
   }
   const gridStoreToSave: Grid = get(grid);
-  localStorage.setItem('bg-generator:grid', JSON.stringify(gridStoreToSave));
+  localStorage.setItem('bg-generator-v1:grid', JSON.stringify(gridStoreToSave));
 }
 
 // Watch for changes to individual grid properties

commit c94537af4b43acccc80dde4317231b8567137a3b

Fix app restoring persisted state
This fixes a bug introduced in aa4f52a.

src/stores/Grid.ts
@@ -84,7 +84,7 @@ export function restoreGrid(): Grid {
       tiles: generateGridTiles({ ...defaultGrid })
     };
   }
-  const rawValue = localStorage.getItem('bg-generator:grid');
+  const rawValue = localStorage.getItem('bg-generator-v1:grid');
   if (rawValue === null) {
     return { ...defaultGrid, tiles: generateGridTiles({ ...defaultGrid }) };
   }